68c0e8f70d63eaae48a33c1c64d2ceceb8670fbb
Furkan Enes Apaydın <feapaydin@gmail.com>
2021-11-05 17:58:57 +0300

    survey show endpoint test complete, response key change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
   def render_success(data)
-    render json: { success: true, data: data }, status: :ok
+    render json: { success: true, payload: data }, status: :ok
   end
 
   def render_bad_request(data)
-    render json: { success: false, data: data }, status: :bad_request
+    render json: { success: false, payload: data }, status: :bad_request
   end
 end
--- spec/requests/survey_spec.rb
+++ spec/requests/survey_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Surveys", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/survey/show"
+RSpec.describe 'Surveys', type: :request do
+  before(:all) do
+    @survey = Survey.first
+  end
+
+  describe 'GET /show/:id' do
+    it 'returns http success' do
+      get "/survey/#{@survey.id}"
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns survey in response' do
+      get "/survey/#{@survey.id}"
+      body = JSON.parse(response.body).with_indifferent_access
+      expect(body[:payload][:id]).to be_truthy
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/survey/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      post '/survey'
       expect(response).to have_http_status(:success)
     end
   end
